40d1de5546cfeb84f51590a9404e3c9150a4f8e8
Sixtiess <liamhhickey@icloud.com>
2024-11-13 15:14:47 -0800

    quick push

--- webapp.py
+++ webapp.py
@@ -4,6 +4,7 @@ from datetime import datetime
 
 import os
 import json
+import time
 
 app = Flask(__name__)
 app.secret_key=os.environ["secret_key"];
@@ -17,6 +18,18 @@ def render_about():
 def render_question1():
     session["qNum"] = 0
     session["uArray"] = []
+    session["startTime"] = time.time()
+    session["time"] = 0
+    session["Finished"] ="false"
+    
+    # if not session["Finished"] == "true":
+        # difference = (time.time() - int(session["startTime"])) * 1000
+        # session["time"] = str(difference)
+    # else:
+        # difference = int(float(session["time"]))
+    
+    
+        
     with open('static/answers.json') as data:
         answers = json.load(data)
         
@@ -27,13 +40,20 @@ def render_question1():
     
     # sets q to question in json file
     q = answers[session["qNum"]]["Question"]
-    return render_template('questionLayout.html',question=q,a=ans,qNum=int(session["qNum"])+1)
+    return render_template('questionLayout.html',question=q,a=ans,qNum=int(session["qNum"])+1,qLength=len(answers),startTime=session["time"])
     
     
     
     
 @app.route('/next',methods=["GET","POST"])
 def render_next():
+    if not session["Finished"] == "true":
+        difference = (time.time() - int(session["startTime"])) *1000
+        session["time"] = str(difference)
+    else:
+        difference = int(float(session["time"]))
+    
+    
     uA = request.form["answer"]
     session["uArray"].append(uA)
     session["qNum"] += 1
@@ -42,7 +62,9 @@ def render_next():
         
     # -1 because qNum starts at 1
     if session["qNum"] + 1 > len(answers):
-        return render_template('end.html',score=calculateScore(session["uArray"]))
+        Score = calculateScore(session["uArray"])
+        session["Finished"] = "true"
+        return render_template('end.html',score=Score,startTime=int(difference))
     # for loop that puts answers from json file into "ans" array
     ans = []
     for i in range(4): # 4 options to choose from
@@ -50,7 +72,7 @@ def render_next():
     
     # sets q to question in json file
     q = answers[session["qNum"]]["Question"]
-    return render_template('questionLayout.html',question=q,a=ans,qNum=int(session["qNum"])+1)
+    return render_template('questionLayout.html',question=q,a=ans,qNum=int(session["qNum"])+1,qLength=len(answers),startTime=int(difference))
 
 
 
